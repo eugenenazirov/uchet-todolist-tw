86d56307e14c543f88597a306e2174adcc29f5fa
Eugene Nazirov <jagzonmusic@gmail.com>
2023-01-25 17:04:24 +0600

    Fix task create email sending

--- task/signals.py
+++ task/signals.py
@@ -6,7 +6,11 @@ from task.models import Task
 
 @receiver(signal=pre_save, sender=Task)
 def on_status_change(instance, **kwargs):
-    owner_id, pk, is_completed_new = instance.owner_id, instance.pk, instance.is_completed
+    pk = instance.pk
+    if not instance.pk:
+        return
+
+    owner_id, is_completed_new = instance.owner_id, instance.is_completed
     is_completed_old = Task.objects.get(id=pk).is_completed
     if is_completed_old != is_completed_new:
         send_mail_on_status_change.delay(instance.owner_id, instance.pk, is_completed_new)
